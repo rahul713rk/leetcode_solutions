ccce1ed810d62e661038dc3a3cbba7ded5ff2659
Rahul Kumar <rahulkumar713rk@gmail.com>
2025-08-07 17:57:01 +0530

    .java Easy 0070-climbing-stairs 0 ms (100%) 40.3 MB (65.73%)

--- LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,14 @@
 class Solution {
-    // Bottom - Up approach
+    // Bottom - Up approach - space optimized
     public int climbStairs(int n) {
         if (n<=2) return n;
-
-        int[] dp = new int[n+1];
-        dp[1] = 1;
-        dp[2] = 2;
-
+        int a = 1;
+        int b = 2;
         for (int i = 3 ; i<=n ; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            int temp = b;
+            b = a+b;
+            a = temp;
         }
-        return dp[n];
+        return b;
     }
 }
