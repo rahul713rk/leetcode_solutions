5d297a98856affdeee86e6284bc4adbefaa5b991
Rahul Kumar <rahulkumar713rk@gmail.com>
2025-08-07 17:57:31 +0530

    .java Easy 0070-climbing-stairs 0 ms (100%) 40.3 MB (82.03%)

--- LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,14 +1,29 @@
 class Solution {
-    // Bottom - Up approach - space optimized
+    //  using matrix exponentiation
     public int climbStairs(int n) {
-        if (n<=2) return n;
-        int a = 1;
-        int b = 2;
-        for (int i = 3 ; i<=n ; i++){
-            int temp = b;
-            b = a+b;
-            a = temp;
+        if (n==1) return 1;
+        int[][] m = {{1,1},{1,0}};
+        int[][] result = matrixPow(m,n);
+        return result[0][0];
+    }
+
+    private int[][] matrixPow(int[][] m , int n){
+        int[][] result = {{1,0},{0,1}};
+        int[][] base =m;
+
+        while (n>0){
+            if (n%2  ==  1){
+                result = matrixmul(result , base);
+            }
+            base = matrixmul(base , base);
+            n /= 2;
         }
-        return b;
+        return result;
+    }
+    private int[][] matrixmul(int[][] a , int[][] b){
+        return new int[][] {
+            {a[0][0]*b[0][0]+a[0][1]*b[1][0] , a[0][0]*b[0][1]+a[0][1]*b[1][1]},
+            {a[1][0]*b[0][0]+a[1][1]*b[1][0] , a[1][0]*b[0][1]+a[1][1]*b[1][1]}
+        };
     }
 }
