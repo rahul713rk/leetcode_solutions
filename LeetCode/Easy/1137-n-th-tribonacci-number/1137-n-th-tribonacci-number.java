298f7f92bd2267241fde11d0c44d69823edb00a7
Rahul Kumar <rahulkumar713rk@gmail.com>
2025-08-08 09:21:14 +0530

    .java Easy 1137-n-th-tribonacci-number 0 ms (100%) 40.6 MB (38.09%)

---
+++ LeetCode/Easy/1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    // top-down approach
+    int[] dp;
+    public int tribonacci(int n) {
+        dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return dfs(n);
+    }
+    private int dfs(int n){
+        if (n<=0) return 0;
+        if (n==1 || n==2) return 1;
+
+        if (dp[n] != -1) return dp[n];
+        return dp[n] = dfs(n-1)+dfs(n-2)+dfs(n-3);
+    }
+}
