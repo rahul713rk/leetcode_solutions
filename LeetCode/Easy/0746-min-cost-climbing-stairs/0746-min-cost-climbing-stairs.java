18e802affa674c79a86a0cdc1c6dd0d9ccf5c551
Rahul Kumar <rahulkumar713rk@gmail.com>
2025-08-08 09:09:37 +0530

    .java Easy 0746-min-cost-climbing-stairs 0 ms (100%) 43.6 MB (13.5%)

--- LeetCode/Easy/0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ LeetCode/Easy/0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,19 +1,11 @@
 class Solution {
-    // top down approach
-    int[] dp;
+    // bottom up approach
     public int minCostClimbingStairs(int[] cost) {
-        dp = new int[cost.length];
-        for (int i =0;i<cost.length;i++){
-            dp[i] = -1;
-        }
-        return Math.min(dfs(cost , 0) , dfs(cost,1));
-    }
-
-    private int dfs(int[] cost , int i){
-        if (i >= cost.length) return 0;
-        if (dp[i] != -1){
-            return dp[i];
-        }
-        return dp[i] = cost[i] + Math.min(dfs(cost , i+1) , dfs(cost , i+2));
+       int n = cost.length;
+       int[] dp = new int[n+1];
+       for (int i=2 ; i<=n ; i++){
+        dp[i] = Math.min(dp[i-1]+cost[i-1] , dp[i-2] + cost[i-2]);
+       }
+       return dp[n];
     }
 }
