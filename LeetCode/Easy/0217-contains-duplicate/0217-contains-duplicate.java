4f6ccadeb4dd7de598e624db1429e303629e287b
Rahul Kumar <rahulkumar713rk@gmail.com>
2025-08-29 08:19:25 +0530

    .java Easy 0217-contains-duplicate 25 ms (6.59%) 59.7 MB (22.35%)

--- LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.java
+++ LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,14 +1,5 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> res = new HashSet<>();
-
-        for (int num : nums){
-            if (!res.add(num)){
-                return true ;
-            }
-            // System.out.println(res.add(num));
-            // // this line print true if res add the num else return false
-        }
-        return false;
+        return Arrays.stream(nums).distinct().count() != nums.length;
     }
 }
