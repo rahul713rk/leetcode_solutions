47477545e53f6fde9d1f1cfeaf3f2dde8e6bdd61
Rahul Kumar <rahulkumar713rk@gmail.com>
2025-11-01 11:17:11 +0530

    .java Medium 0347-top-k-frequent-elements

--- LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,5 +1,10 @@
 class Solution {
-
+    static{
+        Solution sol = new Solution();
+        for(int i =0 ; i<500 ; i++){
+            sol.topKFrequent(new int[] {1} , 1);
+        }
+    }
     // sorting method
     // time complexity - O(nlogn)
     // space complexity - O(n)
