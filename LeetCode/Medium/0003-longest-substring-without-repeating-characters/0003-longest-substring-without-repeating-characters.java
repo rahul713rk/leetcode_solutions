d9dcb4cb7da4bd5267ac6fc8071ffee35cd17256
Rahul Kumar <rahulkumar713rk@gmail.com>
2025-11-11 14:42:34 +0530

    .java Medium 0003-longest-substring-without-repeating-characters

--- LeetCode/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ LeetCode/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -6,14 +6,11 @@ class Solution {
 
         for (int r = 0 ; r<s.length() ; r++){
             char c = s.charAt(r);
-            seen.put(c , seen.getOrDefault(c , 0)+1);
-
-            while (seen.get(c)>1){
-                char temp = s.charAt(l);
-                seen.put(temp , seen.get(temp)-1);
-                l++;
+            if (seen.containsKey(c) && seen.get(c) >= l){
+                l = seen.get(c)+1;
             }
             res = Math.max(res , (r-l)+1);
+            seen.put(c, r);
         }
         return res;
     }
