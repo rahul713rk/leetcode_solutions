521ac2effb6b0f85969e8e3c1676a382a624b15a
Rahul Kumar <rahulkumar713rk@gmail.com>
2026-01-02 11:12:34 +0530

    .java Easy 0027-remove-element

--- LeetCode/Easy/0027-remove-element/0027-remove-element.java
+++ LeetCode/Easy/0027-remove-element/0027-remove-element.java
@@ -1,15 +1,20 @@
 class Solution {
+    static{
+        Solution sol = new Solution();
+        for (int i = 0 ; i<500 ; i++){
+            sol.removeElement(new int[] {} , 0);
+        }
+    }
 
     public int removeElement(int[] nums, int val) {
         int i = 0;
-        int j = nums.length-1;
+        int j = nums.length;
         while (i<j){
             if (nums[i] == val){
-                nums[i] = nums[j];
-                j--;
+                nums[i] = nums[--j];
             }
             else i++;
         }
-        return j+1;
+        return j;
     }
 }
